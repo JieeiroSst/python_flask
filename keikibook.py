d158dd9360fe04b504844909a5f55f93aeb93ae8
Alain Sst <AlainLuu.1999@gmail.com>
2021-05-29 01:52:51 +0700

    URL Building

--- keikibook.py
+++ keikibook.py
@@ -1,31 +1,21 @@
-from flask import Flask
-from markupsafe import escape
+from flask import Flask,url_for
 
 app = Flask(__name__)
 
-# string (default) accepts any text without a slash
-# int accepts positive integers
-# float accepts positive floating point values
-# path like string but also accepts slashes
-# uuid accepts UUID strings
+@app.route('/')
+def index():
+    return 'index'
+
+@app.route('/login')
+def login():
+    return 'login'
 
 @app.route('/user/<username>')
-def show_user_profile(username):
-    return f'User {escape(username)}'
+def profile(username):
+    return f'{username}\'s profile'
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    return f'Post {post_id}'
-
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    return f'Subpath {escape(subpath)}'
-
-@app.route('/projects/')
-def projects():
-    return 'The project page'
-
-@app.route('/about')
-def about():
-    return 'The about page'
-
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
