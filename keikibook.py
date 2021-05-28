b39dce6addb3e8ca7d9dadbb67e8b40138ed6aeb
Alain Sst <AlainLuu.1999@gmail.com>
2021-05-29 02:17:08 +0700

    Responses

--- keikibook.py
+++ keikibook.py
@@ -1,18 +1,12 @@
-from flask import Flask,abort, redirect, url_for,render_template
+from flask import Flask,render_template,make_response
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return redirect(url_for('login'))
-
-@app.route('/login')
-def login():
-    abort(401)
-    this_is_never_executed()
-
-def this_is_never_executed():
-    pass
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('error.html'), 404
 
 @app.errorhandler(404)
-def page_not_found(error):
-    return render_template('page_not_found.html'), 404
+def not_found(error):
+    resp = make_response(render_template('error.html'), 404)
+    resp.headers['X-Something'] = 'A value'
+    return resp
