d1772aa2c0bcc581e9dcb541800084274bfcb18d
Alain Sst <AlainLuu.1999@gmail.com>
2021-05-29 02:06:12 +0700

    File Uploads

--- keikibook.py
+++ keikibook.py
@@ -1,25 +1,45 @@
-from flask import Flask,request
-
+import os
+from flask import Flask,request,flash,redirect, url_for
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    error = None
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
     if request.method == 'POST':
-        if valid_login(request.form['username'],
-                       request.form['password']):
-            return log_the_user_in(request.form['username'])
-        else:
-            error = 'Invalid username/password'
-    # the code below is executed if the request method
-    # was GET or the credentials were invalid
-    return render_template('login.html', error=error)
+        f = request.files['the_file']
+        f.save('/var/www/uploads/uploaded_file.txt')
 
-def valid_login():
-    pass
 
-def log_the_user_in():
-    pass
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
-def render_template():
-    pass
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
