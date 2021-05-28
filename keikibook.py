04bf34dfbb5603a99e35324bb228acd0e679afcf
Alain Sst <AlainLuu.1999@gmail.com>
2021-05-29 01:50:21 +0700

    feature/variable_rules

--- keikibook.py
+++ keikibook.py
@@ -1,11 +1,31 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Index Page'
+# string (default) accepts any text without a slash
+# int accepts positive integers
+# float accepts positive floating point values
+# path like string but also accepts slashes
+# uuid accepts UUID strings
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return f'User {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return f'Post {post_id}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    return f'Subpath {escape(subpath)}'
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
