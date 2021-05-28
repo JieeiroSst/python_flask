847f2ffa7ac1d2b51b925edc076dc22f6cdfc0e5
Alain Sst <AlainLuu.1999@gmail.com>
2021-05-29 01:55:47 +0700

    HTTP Methods

--- keikibook.py
+++ keikibook.py
@@ -1,21 +1,16 @@
-from flask import Flask,url_for
+from flask import Flask,request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'index'
-
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return 'login'
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
 
-@app.route('/user/<username>')
-def profile(username):
-    return f'{username}\'s profile'
+def do_the_login():
+    pass
 
-with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('login'))
-    print(url_for('login', next='/'))
-    print(url_for('profile', username='John Doe'))
+def show_the_login_form():
+    pass
